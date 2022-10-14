5f56a320c7b3b0e1333e40509740450b28bb748f
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2022-10-14 20:07:50 +0100

    2 spheres

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,38 @@ use ray::Ray;
 use scene::Scene;
 use vec3::{Color, Point3, Vec3};
 
+#[derive(Default)]
+struct World(Vec<Sphere>);
+
 fn main() {
     let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "raaaaaaayz",
-        options,
-        Box::new(|_cc| Box::new(MyApp::default())),
-    );
+    let world = World(vec![
+        Sphere {
+            center: Vec3::from([0.0, 0.0, -1.0]),
+            radius: 0.5,
+        },
+        Sphere {
+            center: Vec3::from([0.0, -100.5, -1.0]),
+            radius: 100.0,
+        },
+    ]);
+
+    let app = MyApp {
+        world,
+        ..Default::default()
+    };
+
+    eframe::run_native("raaaaaaayz", options, Box::new(|_cc| Box::new(app)));
 }
 
 #[derive(Default)]
 struct MyApp {
+    world: World,
     display: Option<(Scene, RetainedImage)>,
     last_size: egui::Vec2,
 }
 
-fn gen_scene(ui: &egui::Ui) -> Scene {
-    let size = ui.available_size();
+fn gen_scene(size: &egui::Vec2) -> Scene {
     let aspect_ratio = if size.y == 0.0 { 0.0 } else { size.x / size.y };
 
     let viewport_height = 2.0;
@@ -50,14 +65,14 @@ impl eframe::App for MyApp {
             }
 
             let img = self.display.get_or_insert_with(|| {
-                let scene = gen_scene(ui);
-                let img = gen_image(&scene);
+                let scene = gen_scene(&ui.available_size());
+                let img = gen_image(&self.world, &scene);
                 (scene, img)
             });
 
             if self.last_size != size {
-                let scene = gen_scene(ui);
-                let i = gen_image(&scene);
+                let scene = gen_scene(&ui.available_size());
+                let i = gen_image(&self.world, &scene);
                 *img = (scene, i);
             };
 
@@ -67,18 +82,23 @@ impl eframe::App for MyApp {
     }
 }
 
-fn ray_color(ray: &Ray) -> Color {
-    let t = hit_sphere(&Vec3::from([0.0, 0.0, -1.0]), 0.5, ray);
-    if t > 0.0 {
-        let n = (ray.at(t) - Vec3::from([0.0, 0.0, -1.0])).unit(); // sphere center
-        return 0.5 * Color::from([n.x + 1.0, n.y + 1.0, n.z + 1.0]);
+fn ray_color<T>(world: T, ray: &Ray) -> Color
+where
+    T: Hittable,
+{
+    match world.hit(ray, 0.0, f64::INFINITY) {
+        Some(hit) => {
+            0.5 * (hit.normal + Color::from([1.0, 1.0, 1.0]))
+        }
+        None => {
+            let unit_direction = ray.dir.unit();
+            let t = 0.5 * (unit_direction.y + 1.0);
+            (1.0 - t) * Color::from([1.0, 1.0, 1.0]) + t * Color::from([0.5, 0.7, 1.0])
+        }
     }
-    let unit_direction = ray.dir.unit();
-    let t = 0.5 * (unit_direction.y + 1.0);
-    (1.0 - t) * Color::from([1.0, 1.0, 1.0]) + t * Color::from([0.5, 0.7, 1.0])
 }
 
-fn gen_image(scene: &Scene) -> RetainedImage {
+fn gen_image(world: &World, scene: &Scene) -> RetainedImage {
     let start = std::time::Instant::now();
     let size = [scene.image_width, scene.image_height];
     let width = size[0];
@@ -98,7 +118,7 @@ fn gen_image(scene: &Scene) -> RetainedImage {
                 dir,
             };
 
-            ray_color(&ray).into()
+            ray_color(world, &ray).into()
         })
         .collect();
 
@@ -109,16 +129,88 @@ fn gen_image(scene: &Scene) -> RetainedImage {
     image
 }
 
-fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> f64 {
-    let oc = ray.orig - center;
-    let a = ray.dir.dot(&ray.dir);
-    let b = 2.0 * oc.dot(&ray.dir);
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+#[derive(Debug)]
+enum Face {
+    Front,
+    Back,
+}
 
-    if discriminant < 0.0 {
-        -1.0 // no hit
-    } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+#[derive(Debug)]
+struct HitRecord {
+    p: Point3,
+    normal: Vec3,
+    t: f64,
+    face: Face,
+}
+
+impl HitRecord {
+    fn new(p: Point3, outward_normal: Vec3, t: f64, ray: &Ray) -> Self {
+        let (normal, face) = if ray.dir.dot(&outward_normal) > 0.0 {
+            (-outward_normal, Face::Back)
+        } else {
+            (outward_normal, Face::Front)
+        };
+        Self { p, normal, t, face }
+    }
+}
+
+trait Hittable {
+    fn hit(&self, ray: &Ray, tmin: f64, tmax: f64) -> Option<HitRecord>;
+}
+
+#[derive(Debug, Default)]
+struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray, tmin: f64, tmax: f64) -> Option<HitRecord> {
+        let oc = ray.orig - self.center;
+        let a = ray.dir.length_squared();
+        let half_b = oc.dot(&ray.dir);
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrtd = discriminant.sqrt();
+        // find the nearest root that lies in the acceptable range
+        let mut root = (-half_b - sqrtd) / a;
+        if root < tmin || tmax < root {
+            root = (-half_b + sqrtd) / a;
+            if root < tmin || tmax < root {
+                return None;
+            }
+        }
+
+        let p = ray.at(root);
+        let outward_normal = (p - self.center) / self.radius;
+        Some(HitRecord::new(p, outward_normal, root, ray))
+    }
+}
+
+impl<T> Hittable for Vec<T>
+where
+    T: Hittable,
+{
+    fn hit(&self, ray: &Ray, tmin: f64, tmax: f64) -> Option<HitRecord> {
+        let mut closest_so_far = tmax;
+        let mut hit = None;
+
+        for obj in self {
+            if let Some(obj_hit) = obj.hit(ray, tmin, closest_so_far) {
+                closest_so_far = obj_hit.t;
+                hit = Some(obj_hit);
+            }
+        }
+        hit
+    }
+}
+
+impl<'a> Hittable for &'a World {
+    fn hit(&self, ray: &Ray, tmin: f64, tmax: f64) -> Option<HitRecord> {
+        self.0.hit(ray, tmin, tmax)
     }
 }
